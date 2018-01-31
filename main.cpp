7b80d4dcb125c210bfb751d88c2f800b08913bbf
bmanga <bruno.manga95@gmail.com>
2018-01-31 15:00:42 +0000

    test the ultrasonic sensors with blocking code

--- main.cpp
+++ main.cpp
@@ -1,4 +1,54 @@
+#include "mbed.h"
+
+DigitalOut trigger(p6);
+DigitalOut myled(LED1); //monitor trigger
+DigitalOut myled2(LED2); //monitor echo
+DigitalIn  echo(p7);
+Serial pc(USBTX, USBRX);
+int distance = 0;
+int correction = 0;
+Timer sonar;
+
 int main()
 {
-	return 0;
+    sonar.reset();
+// measure actual software polling timer delays
+// delay used later in time correction
+// start timer
+    sonar.start();
+// min software polling delay to read echo pin
+    while (echo==2) {};
+    myled2 = 0;
+// stop timer
+    sonar.stop();
+// read timer
+    correction = sonar.read_us();
+    pc.printf("Approximate software overhead timer delay is %d uS\n\r",correction);
+ 
+//Loop to read Sonar distance values, scale, and print
+    while(1) {
+// trigger sonar to send a ping
+        trigger = 1;
+        myled = 1;
+        myled2 = 0;
+        sonar.reset();
+        wait_us(10.0);
+        trigger = 0;
+        myled = 0;
+//wait for echo high
+        while (echo==0) {};
+        myled2=echo;
+//echo high, so start timer
+        sonar.start();
+//wait for echo low
+        while (echo==1) {};
+//stop timer and read value
+        sonar.stop();
+//subtract software overhead timer delay and scale to cm
+        distance = (sonar.read_us()-correction)/58.0;
+        myled2 = 0;
+        pc.printf(" %d cm \n\r",distance);
+//wait so that any echo(s) return before sending another ping
+        wait(0.2);
+    }
 }
